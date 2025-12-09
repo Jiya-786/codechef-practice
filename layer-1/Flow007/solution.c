153a5222d58354b835e85b024f241604f4c8ec37
Jiya-786 <merjajiya@gmail.com>
2025-12-09 16:06:10 +0530

    Add logic to remove trailing zeros before reversing number
    - Added loop to strip trailing zeros (while n % 10 == 0)
    - Ensures reversed output does not include unnecessary leading zeros
    - Updated reversing logic to use cleaned value of n
    - Improves correctness for inputs like 12000, 3050, 100, etc.

--- layer-1/Flow007/solution.c
+++ layer-1/Flow007/solution.c
@@ -9,8 +9,15 @@ int main(){
         int n;
         scanf("%d",&n);
         
+        // we will have to remove trailing zeroes so remove zeroes at the ending of the number and then start reversing
+        
+        while(n%10==0 && n!=0){
+            n=n/10;
+        }
+
         int digit=n;
         while(digit>0){
+            
             printf("%d",digit%10);
             digit=digit/10;
         }
