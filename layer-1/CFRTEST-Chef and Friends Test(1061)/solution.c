40436260029e770d0533832476976895abcbe328
Jiya-786 <merjajiya@gmail.com>
2025-12-11 15:29:21 +0530

    added soln for CFRTEST

--- layer-1/CFRTEST-Chef and Friends Test(1061)/solution.c
+++ layer-1/CFRTEST-Chef and Friends Test(1061)/solution.c
@@ -4,53 +4,59 @@ int main(){
     
     int t;
     scanf("%d",&t);
-
-    while(t--){
+    for(int i=0;i<t;i++){
         int n;
         scanf("%d",&n);
+        
         int a[n];
-        int b[n];
-        
-        for (int i=0;i<n;i++){
-            scanf(" %d",&a[i]);}            // if we write only "%c, it will consider spaces                                 also as characters. if we write " %c", it will                                 delete leading spaces and consider only the                                    characters as characters no spaces.
-                                             // but this not hold for %d or anything else.     others will always condier the same and not consider space as characters regardless of inout has space or not
-       for (int i=0;i<n;i++){
-            scanf(" %d",&b[i]);}       
-        
-        int best_time_a=0;
-        int best_time_b=0;
-        for (int i=0;i<n;i++){
-            if (a[i]>best_time_a){
-                best_time_a=a[i];
-            }
-            if (b[i]>best_time_b){
-                best_time_b=b[i];
-            }
+        for(int i=0;i<n;i++){
+            scanf("%d",&a[i]);
         }
         
-        int sum_of_a=0;
-        int sum_of_b=0;
-        for (int i=0;i<n;i++){
-            sum_of_a+=a[i];
-            sum_of_b+=b[i];
-        }
-        
-        sum_of_a=sum_of_a-best_time_a;
-        sum_of_b=sum_of_b-best_time_b;
-        
-        if(sum_of_a>sum_of_b){
-            printf("Bob\n");
-        }
-        else if(sum_of_a<sum_of_b){
-            printf("Alice\n");
-        }
-        else{
-            printf("Draw\n");
+        int count[100];
+        for (int i=0;i<100;i++){
+            count[i]=0;
         }
         
         
+        for(int i=0;i<n;i++){
+            count[a[i]-1]++;
+        }
+        
+        int counter=0;
+        for(int i=0;i<100;i++){
+            if(count[i]>=1){
+                counter++;
+        }
     }
-    
-    
-    return 0;
+    printf("%d\n",counter);
 }
+}
+
+// #include <stdio.h>
+
+// int main(){
+    
+//     int t;
+//     scanf("%d",&t);
+    
+//     for(int i=0;i<t;i++){
+//         int n;
+//         scanf("%d",&n);
+        
+//         int a[n];
+//         for(int i=0;i<n;i++){
+//             scanf("%d",&a[i]);
+//         }
+        
+//         int new_list[n];
+//         for(int i=0;i<n;i++){
+//             if a[i] not in new_list{
+//                 new_list.append(a[i]);
+//             }
+//         }
+    
+    
+    
+//     return 0;
+// }
