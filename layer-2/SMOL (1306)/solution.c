e86e36946265f0ce62c2cde94b60f18e4b256200
Jiya-786 <merjajiya@gmail.com>
2025-12-11 19:14:53 +0530

    Simplified the solution logic based on the observation that repeatedly subtracting K from N is equivalent to computing N % K.
    Replaced the previous iterative/conditional approach with a direct modulo operation.
    
    This reduces the code complexity and produces the correct smallest non-negative value efficiently for all test cases.

--- layer-2/SMOL (1306)/solution.c
+++ layer-2/SMOL (1306)/solution.c
@@ -1,3 +1,23 @@
+// #include <stdio.h>
+
+// int main() {
+// 	int t;
+// 	scanf("%d",&t);
+// 	while(t--){
+// 	    int n,k;
+// 	    scanf("%d %d",&n,&k);
+// 	    if(k==0){
+// 	        printf("%d\n",n);
+// 	    }
+// 	    while(n>=0 && k!=0){
+// 	        n=n-k;
+// 	    }
+// 	    printf("%d\n",n+k);
+// 	}
+// }
+
+// Your code works correctly for the given test cases, but the logic can be simplified significantly. The problem asks for the smallest non-negative integer you can obtain by subtracting K from N any number of times. This is essentially the same as finding the remainder when N is divided by K.
+
 #include <stdio.h>
 
 int main() {
@@ -9,10 +29,10 @@ int main() {
 	    if(k==0){
 	        printf("%d\n",n);
 	    }
-	    while(n>=0 && k!=0){
-	        n=n-k;
+	    else{
+	        printf("%d\n",n%k);
 	    }
-	    printf("%d\n",n+k);
+	    
 	}
+	    
 }
-
