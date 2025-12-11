78b9ede6fe7e7bef0196e6f8cd7eb286bf2ccdfd
Jiya-786 <merjajiya@gmail.com>
2025-12-11 18:34:21 +0530

    old code will not work if two inputs are same so this better version there is also another method in whihc we can assign max and 2nd max in just one loop using the format:
            int max_num, second_max;
    
            if (a >= b && a >= c) {
                max_num = a;

--- layer-2/CHFSPL (945)/solution.c
+++ layer-2/CHFSPL (945)/solution.c
@@ -1,30 +1,55 @@
+// #include <stdio.h>
+
+// int main() {
+// 	int t;
+// 	scanf("%d",&t);
+	
+// 	while(t--){
+// 	    int a,b,c;
+// 	    int numbers[3];
+// 	    for(int i=0;i<3;i++){
+// 	        scanf("%d",&numbers[i]);
+// 	    }
+// 	    int max_number=numbers[0];
+// 	    for (int i=0;i<3;i++){
+// 	        if(numbers[i]>max_number){
+// 	            max_number=numbers[i];
+// 	        }
+// 	    }
+// 	    int second_max=numbers[0];
+// 	        for(int i=0;i<3;i++){
+// 	            if(numbers[i]>second_max && numbers[i]!=max_number){
+// 	                second_max=numbers[i];
+// 	            }
+// 	        }
+	    
+// 	    printf("%d\n",max_number+second_max);
+// 	}
+
+// }
+
 #include <stdio.h>
 
-int main() {
-	int t;
+int main(){
+    
+    int t;
 	scanf("%d",&t);
 	
 	while(t--){
 	    int a,b,c;
-	    int numbers[3];
+	    scanf("%d%d%d",&a,&b,&c);
+	    int sum[3];
+	    sum[0]=a+b;
+	    sum[1]=b+c;
+	    sum[2]=a+c;
+	    int max_sum=0;
 	    for(int i=0;i<3;i++){
-	        scanf("%d",&numbers[i]);
-	    }
-	    int max_number=numbers[0];
-	    for (int i=0;i<3;i++){
-	        if(numbers[i]>max_number){
-	            max_number=numbers[i];
+	        if(sum[i]>max_sum){
+	            max_sum=sum[i];
 	        }
 	    }
-	    int second_max=numbers[0];
-	        for(int i=0;i<3;i++){
-	            if(numbers[i]>second_max && numbers[i]!=max_number){
-	                second_max=numbers[i];
-	            }
-	        }
 	    
-	    printf("%d\n",max_number+second_max);
-	}
-
+	    printf("%d\n",max_sum);
+}
 }
 
