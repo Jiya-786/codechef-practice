bbf81c9f348fbe599c4f5b83d8e89a3b1c8ce775
Jiya-786 <merjajiya@gmail.com>
2025-12-12 17:35:54 +0530

    Changed the min/max starting values to INT_MAX and INT_MIN instead of using big random numbers.
    Makes the code safer and works better for all input ranges.
    
    Keeps the same logic, just a cleaner and more correct way to find min and max.

--- layer-2/MAKEEQUAL (1267)/solution.c
+++ layer-2/MAKEEQUAL (1267)/solution.c
@@ -1,13 +1,41 @@
-//1 3 1 2 3 4 7 5
-//2 4 2 3 4 5 7 6
-//3 5 3 4 5 6 7 7
-//4 6 4 5 6 7 7 7
-//5 7 5 6 7 7 7 7
-//6 7 6 7 7 7 7 7
-//7 7 7 7 7 7 7 7
-//the min number of moves is just the difference between the max and min elemnt of the array and zero moves if all elts are equal.
+// //1 3 1 2 3 4 7 5
+// //2 4 2 3 4 5 7 6
+// //3 5 3 4 5 6 7 7
+// //4 6 4 5 6 7 7 7
+// //5 7 5 6 7 7 7 7
+// //6 7 6 7 7 7 7 7
+// //7 7 7 7 7 7 7 7
+// //the min number of moves is just the difference between the max and min elemnt of the array and zero moves if all elts are equal.
 
+// #include <stdio.h>
+
+// int main() {
+// 	int t;
+// 	scanf("%d",&t);
+// 	while(t--){
+// 	    int n;
+// 	    scanf("%d",&n);
+// 	    int a[n];
+// 	    for(int i=0;i<n;i++){
+// 	        scanf("%d",&a[i]);
+// 	    }
+// 	    int min_elt=100000;
+// 	    int max_elt=-100000;
+// 	    for(int i=0;i<n;i++){
+// 	        if(a[i]>max_elt) max_elt=a[i];
+// 	        if(a[i]<min_elt) min_elt=a[i];
+// 	    }
+// 	    if(max_elt==min_elt){
+// 	        printf("%d\n",0);
+// 	    }
+// 	    else printf("%d\n",max_elt-min_elt);
+// 	}
+// }
+
+
+/*better way is to use INT_MAX and INT_MIN*/
 #include <stdio.h>
+#include <limits.h>
 
 int main() {
 	int t;
@@ -19,8 +47,8 @@ int main() {
 	    for(int i=0;i<n;i++){
 	        scanf("%d",&a[i]);
 	    }
-	    int min_elt=100000;
-	    int max_elt=-100000;
+	    int min_elt=INT_MAX;
+	    int max_elt=INT_MIN;
 	    for(int i=0;i<n;i++){
 	        if(a[i]>max_elt) max_elt=a[i];
 	        if(a[i]<min_elt) min_elt=a[i];
@@ -31,5 +59,3 @@ int main() {
 	    else printf("%d\n",max_elt-min_elt);
 	}
 }
-
-
