8a1d8f4e1e8a0ef5c27b335c87024fe05daf2630
Jiya-786 <merjajiya@gmail.com>
2025-12-14 23:36:31 +0530

    Refactor code to use hashing instead of nested loops
    The earlier code worked correctly.
    This version uses a simple hashing method with a boolean array to store
    which letters are known, and then checks each word using direct lookup.
    This makes the code cleaner and faster while keeping the same result.

--- layer-2/ALPHABET (1123)/solution.c
+++ layer-2/ALPHABET (1123)/solution.c
@@ -29,69 +29,105 @@
 // }
 
 
+/* correct code using boolean flags */
+
+// #include <stdio.h>
+// #include <string.h>
+// #include <stdbool.h>
+
+// int main() {
+
+//     // s stores the letters Jeff knows
+//     // size 27 = 26 letters + null character
+//     char s[27];
+
+//     // read the string of known letters
+//     scanf("%s", s);
+
+//     // length of string s
+//     int length_1 = strlen(s);
+
+//     // number of words to check
+//     int n;
+//     scanf("%d", &n);
+
+//     // process each word
+//     while (n--) {
+
+//         // w stores the current word
+//         // size 13 because max word length is 12 + '\0'
+//         char w[13];
+//         scanf("%s", w);
+
+//         // length of current word
+//         int length_2 = strlen(w);
+
+//         // assume the word can be read
+//         bool canRead = true;
+
+//         // check each letter of the word
+//         for (int i = 0; i < length_2; i++) {
+
+//             // assume current letter is not found in s
+//             bool found = false;
+            
+//             // search for w[i] in string s
+//             for (int j = 0; j < length_1; j++) {
+//                 if (w[i] == s[j]) {
+//                     // letter found in s
+//                     found = true;
+//                     break;  /* stop searching for this
+//                               letter continue to next i. */
+//                 }
+//             }
+
+//             // if this letter is not present in s
+//             if (!found) {
+//                 // word cannot be read
+//                 canRead = false;
+//                 break;  // no need to check remaining letters
+//             }
+//         }
+
+//         // print result for the current word
+//         if (canRead) {
+//             printf("Yes\n");
+//         } else {
+//             printf("No\n");
+//         }
+//     }
+// }
+
+
+/* correct code using hashing */
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
 
-int main() {
-
-    // s stores the letters Jeff knows
-    // size 27 = 26 letters + null character
+int main(){
     char s[27];
-
-    // read the string of known letters
-    scanf("%s", s);
-
-    // length of string s
-    int length_1 = strlen(s);
-
-    // number of words to check
+    scanf("%s",s);
+    
+    bool known[26]={false};
+    for(int i=0;s[i]!='\0';i++){
+        known[s[i]-'a']=true;
+    }
+    
     int n;
-    scanf("%d", &n);
-
-    // process each word
-    while (n--) {
-
-        // w stores the current word
-        // size 13 because max word length is 12 + '\0'
+    scanf("%d",&n);
+    
+    while(n--){
         char w[13];
-        scanf("%s", w);
-
-        // length of current word
-        int length_2 = strlen(w);
-
-        // assume the word can be read
-        bool canRead = true;
-
-        // check each letter of the word
-        for (int i = 0; i < length_2; i++) {
-
-            // assume current letter is not found in s
-            bool found = false;
-            
-            // search for w[i] in string s
-            for (int j = 0; j < length_1; j++) {
-                if (w[i] == s[j]) {
-                    // letter found in s
-                    found = true;
-                    break;  /* stop searching for this
-                               letter continue to next i. */
-                }
-            }
-
-            // if this letter is not present in s
-            if (!found) {
-                // word cannot be read
-                canRead = false;
-                break;  // no need to check remaining letters
+        scanf("%s",w);
+        
+        bool canRead=true;
+        for(int i=0;w[i]!='\0';i++){
+            if(!known[w[i]-'a']){
+                canRead=false;
+                break;
             }
         }
-
-        // print result for the current word
-        if (canRead) {
-            printf("Yes\n");
-        } else {
-            printf("No\n");
-        }
+        if(canRead) printf("Yes\n");
+        else printf("No\n");
     }
 }
