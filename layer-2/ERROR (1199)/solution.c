653b2fc41ac92b0b5ddf70174c9e49db2c1aa890
Jiya-786 <merjajiya@gmail.com>
2025-12-14 11:09:22 +0530

    added soln and readme for ERROR

---
+++ layer-2/ERROR (1199)/solution.c
@@ -0,0 +1,24 @@
+//10010011101
+
+#include <stdio.h>
+
+#include <string.h>
+
+int main() {
+    int t;
+    scanf("%d", & t);
+    while (t--) {
+        char s[100001];
+        scanf("%s", s);
+        int count = 0;
+        for (int i = 0; i < strlen(s) - 2; i++) {
+            if ((s[i] == '0' && s[i + 1] == '1' && s[i + 2] == '0') || (s[i] == '1' && s[i + 1] == '0' && s[i + 2] == '1')) {
+                count++;
+            }
+
+        }
+        if (count > 0) printf("Good\n");
+        else printf("Bad\n");
+
+    }
+}
